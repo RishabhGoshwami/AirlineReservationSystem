b3772c7d4ccff26d961a685d1cb5daa6c1afd66e
RishabhGoshwami <rishabhgoshwami570@gmail.com>
2024-07-06 12:56:29 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,16 @@
+package airlinemanagementsystem;
+import java.sql.*;
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    public Conn(){
+        try  {
+            Class .forName("com.mysql.cj.jdbc.Driver");
+            c =  DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem" ,"root","@Rishabh0505");
+            s = c.createStatement();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
